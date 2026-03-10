398894f0d3c54553918cc3f0da481dd0148a15fc
Aristo Yongka <aristoyongka005@gmail.com>
2026-03-10 13:51:23 +0700

    test: add localStorage.set item mock have been callled with

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -36,6 +36,11 @@ describe("test suite: addToCart", () => {
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
     expect(cart[0].quantity).toEqual(2);
+    expect(localStorage.setItem).toHaveBeenCalledWith("cart", JSON.stringify([{
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+      deliveryOptionId: "1"
+    }]));
   });
 
   it("adds a new product to the cart", () => {
@@ -44,5 +49,14 @@ describe("test suite: addToCart", () => {
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(cart[1].productId).toEqual("15b6fc6f-327a-4ec4-896f-486349e85a3d");
     expect(cart[1].quantity).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith("cart", JSON.stringify([{
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 1,
+      deliveryOptionId: "1"
+    }, {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+      deliveryOptionId: "1"
+    }]));
   });
 });
