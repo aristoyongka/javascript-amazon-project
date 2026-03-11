4a87a3d2c2da109f0b722cb1e7251bf544d1fce2
Aristo Yongka <aristoyongka005@gmail.com>
2026-03-11 16:23:22 +0700

    refactor: convert checkout code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,11 +2,23 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import "../data/cart-class.js";
 // import "../data/backend-practice.js";
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve();
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  }),
+]).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
-  renderCheckoutHeader();
+  renderCheckoutHeader(); 
 });
